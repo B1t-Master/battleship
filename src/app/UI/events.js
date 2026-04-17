9f11bffeb039fae8a021deb756f9b1e7e7046141
B1t-Master <isaac.njuguna@strathmore.edu>
2026-04-17 13:25:35 +0300

    Implement restart game button

--- src/app/UI/events.js
+++ src/app/UI/events.js
@@ -7,6 +7,12 @@ import {
 import { player, computer, inValidChoices } from "../barrel";
 import { createCells, drawBoard } from "./layout";
 import { Gameboard } from "../gameboard";
+function restartGame() {
+  const restartButton = document.querySelector("#restart");
+  restartButton.addEventListener("click", () => {
+    window.location.reload();
+  });
+}
 function randomizeShips() {
   const randomizeButton = document.querySelector("#place-ships");
 
@@ -20,6 +26,7 @@ function randomizeShips() {
 export default function addEvents() {
   let computerGameboard = document.querySelector(".gameboard-2");
   randomizeShips();
+  restartGame();
   computerGameboard.addEventListener("click", () => {
     computerTurn(player, computer, inValidChoices);
   });
--- src/index.js
+++ src/index.js
@@ -3,9 +3,3 @@ import { createGame } from "./app/playgame";
 import { player, computer } from "./app/barrel";
 
 createGame(player, computer);
-
-/*todo
-restart game
-need a mechanism to prevent double hits for players
-further along if its a successful hit place another ship
-*/
